763b2fc670bfcbf9b1f7ff6bb7113e6dab6af997
Diogo Gil <diogo.designer.gil@gmail.com>
2022-01-15 01:35:33 +0000

    Criar Menu Hamburger

--- Client/src/layout/header/Header.js
+++ Client/src/layout/header/Header.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import Logo from "../../asset/img/logo.png";
 import "./header.css";
+import Menu from "./menu/Menu";
 
 export class Header extends Component {
   render() {
     return (
       <div className="header">
+        {/* Menu tipo hambuger */}
+        <Menu />
         {/* Logo */}
         <div className="logo">
           <a href="index">
@@ -13,8 +16,6 @@ export class Header extends Component {
             <h1>Reveratel</h1>
           </a>
         </div>
-
-        {/* Menu tipo hambuger */}
       </div>
     );
   }
---
+++ Client/src/layout/header/menu/Menu.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./menu.css";
+
+function Menu() {
+  //   const open = document.ready(function () {
+  //     "#nav-icon3".click(function () {
+  //       this.toggleClass("open");
+  //     });
+  //   });onClick={open}
+  return (
+    <div id="nav-icon3">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+}
+
+export default Menu;
