84a5f700022ddfe072faf97d0715f150e167cc47
Diogo Gil <diogo.designer.gil@gmail.com>
2022-01-26 01:04:33 +0000

    Home / Typing Animation

--- Client/src/page/Home.js
+++ Client/src/page/Home.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "./home.css";
 
 function Home() {
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <section className="home" id="home">
+      <div className="max-width">
+        <div className="home-content">
+          <div className="static-txt-1">Hello, my name is</div>
+          <div className="static-txt-2">Diogo "Reveratel" Gil</div>
+          <div className="wrapper">
+            <div className="static-txt-3">And I'm a</div>
+            <ul className="dynamic-txts">
+              <li>
+                <span>Designer</span>
+              </li>
+              <li>
+                <span>Developer</span>
+              </li>
+              <li>
+                <span>Freelancer</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
