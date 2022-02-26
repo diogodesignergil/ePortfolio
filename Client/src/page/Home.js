7e54660991f139646c72ac82ae92aa1e4aa86c72
Pedro Monteiro <pedro.monteiro@decode.pt>
2022-02-26 15:53:26 +0000

    Add Translation to Home Page

--- Client/src/page/Home.js
+++ Client/src/page/Home.js
@@ -4,6 +4,8 @@ import { Button } from "../components/Button";
 import "./home.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+// Translation
+import { useTranslation } from "react-i18next";
 
 AOS.init({
   // Global settings:
@@ -27,6 +29,8 @@ AOS.init({
 });
 
 function Home() {
+  const [t] = useTranslation();
+
   return (
     <section className="home" id="home">
       <div className="max-width">
@@ -35,10 +39,10 @@ function Home() {
           data-aos="fade-right"
           data-aos-easing="ease-in-out"
         >
-          <div className="static-txt-1">Hello, my name is</div>
+          <div className="static-txt-1">{t("home.text1")}</div>
           <div className="static-txt-2">Diogo "Reveratel" Gil</div>
           <div className="wrapper">
-            <div className="static-txt-3">And I'm a</div>
+            <div className="static-txt-3">{t("home.text3")}</div>
             <ul className="dynamic-txts">
               <li>
                 <span>Designer</span>
@@ -59,7 +63,7 @@ function Home() {
                 buttonSize="btn--large"
               >
                 <i className="fas fa-download" />
-                &nbsp;DOWNLOAD CV
+                &nbsp;{t("home.download")}
               </Button>
             </a>
             <Link to="/contact">
@@ -68,7 +72,7 @@ function Home() {
                 buttonStyle="btn--primary"
                 buttonSize="btn--large"
               >
-                HIRE ME
+                {t("home.hire")}
               </Button>
             </Link>
           </div>
