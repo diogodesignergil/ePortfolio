ffbac5737e1b664c2ea977bd2a51896ea4ecb9cf
Diogo Gil <diogo.designer.gil@gmail.com>
2022-02-26 11:35:44 +0000

    Contact Form Update

--- Client/src/page/Contact.js
+++ Client/src/page/Contact.js
@@ -1,8 +1,24 @@
 import React from "react";
 import "./contact.css";
 import { Button } from "../components/Button";
+import emailjs from "emailjs-com";
 
 function Contact() {
+	function sendEmail(e) {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_5n2vtk8",
+				"template_kqa4j7u",
+				e.target,
+				"user_f2ecmxnUHgpBDOPsRCC9l"
+			)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => console.log(err));
+	}
 	return (
 		<section className="contact">
 			<h1>Contact</h1>
@@ -14,19 +30,21 @@ function Contact() {
 			<p className="contact-note">
 				Entrar em contacto comigo, por favor usar o formulário em baixo.
 			</p>
-			<form>
-				<input type="text" placeholder="Nome" required />
-				<input type="email" placeholder="Email" required />
-				<input type="text" placeholder="Assunto" required />
-				<textarea placeholder="Mensagem" required></textarea>
-				<Button
-					className="btns"
-					buttonStyle="btn--primary"
-					buttonSize="btn--large"
-					type="submit"
-				>
-					SUBMETER
-				</Button>
+			<form onSubmit={sendEmail}>
+				<input type="text" placeholder="Nome" name="name" required />
+				<input type="email" placeholder="Email" name="user_email" required />
+				<input type="text" placeholder="Assunto" name="subject" required />
+				<textarea placeholder="Mensagem" name="message" required></textarea>
+				<div className="button-contianer">
+					<Button
+						className="btns"
+						buttonStyle="btn--primary"
+						buttonSize="btn--large"
+						type="submit"
+					>
+						SUBMETER
+					</Button>
+				</div>
 			</form>
 		</section>
 	);
