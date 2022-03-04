e60c27236fcafcbe56ce76ae9171d65fdee4b8eb
Pedro Monteiro <pedro.monteiro@decode.pt>
2022-03-04 17:47:21 +0000

    Update Language Dropdown Menu

--- Client/src/components/layout/header/language/language.js
+++ Client/src/components/layout/header/language/language.js
@@ -1,22 +1,54 @@
 import "./language.css";
-import { React } from "react";
+import { React, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function LanguageMenu() {
-	const [t, i18n] = useTranslation();
+  const [t, i18n] = useTranslation();
+  const [getLanguage, setLanguage] = useState(
+    localStorage.getItem("i18nextLng") === "en"
+      ? "menu.languages.english"
+      : "menu.languages.portuguese"
+  );
+  const [getActive, setActive] = useState(
+    localStorage.getItem("i18nextLng") === "en" ? "en" : "pt"
+  );
 
-	const handleLanguageChange = (event) => {
-		i18n.changeLanguage(event.target.value);
-	};
+  const handleLanguageChange = (language) => {
+    i18n.changeLanguage(language);
+    setActive(language);
+    setLanguage(
+      language === "en" ? "menu.languages.english" : "menu.languages.portuguese"
+    );
+  };
 
-	return (
-		<div className="language-contianer">
-			<select onChange={handleLanguageChange}>
-				<option value={"pt"}>{t("menu.languages.portuguese")}</option>
-				<option value={"en"}>{t("menu.languages.english")}</option>
-			</select>
-		</div>
-	);
+  return (
+    <div className="nav-wrapper">
+      <div className="sl-nav">
+        {t("menu.languages.choosenLanguage")}:{" "}
+        <ul>
+          <li>
+            <b>{t(getLanguage)}</b>{" "}
+            <i className="fa fa-angle-down" aria-hidden="true"></i>
+            <div className="triangle"></div>
+            <ul>
+              <li onClick={() => handleLanguageChange("pt")}>
+                <i className="sl-flag flag-pt"></i>
+                <span className={getActive === "pt" ? "active" : ""}>
+                  {t("menu.languages.portuguese")}
+                </span>
+              </li>
+              <li onClick={() => handleLanguageChange("en")}>
+                <i class="sl-flag flag-en"></i>
+                <span className={getActive === "en" ? "active" : ""}>
+                  {t("menu.languages.english")}
+                </span>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default LanguageMenu;
