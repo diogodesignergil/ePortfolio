5933d44c45ab3ea5b8d28381d71016a3dae7c0ba
Diogo Gil <diogo.designer.gil@gmail.com>
2022-03-07 21:51:51 +0000

    Services Card

--- Client/src/page/Home.js
+++ Client/src/page/Home.js
@@ -2,34 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
 import "./home.css";
-import AOS from "aos";
-import "aos/dist/aos.css";
 // Translation
 import { useTranslation } from "react-i18next";
 
 import CV from "../asset/document/CV-DiogoGil.pdf";
 import Ring from "../asset/img/ring.png";
-
-AOS.init({
-	// Global settings:
-	disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-	startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
-	initClassName: "aos-init", // class applied after initialization
-	animatedClassName: "aos-animate", // class applied on animation
-	useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-	disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-	debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-	throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-
-	// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-	offset: 120, // offset (in px) from the original trigger point
-	delay: 0, // values from 0 to 3000, with step 50ms
-	duration: 400, // values from 0 to 3000, with step 50ms
-	easing: "ease", // default easing for AOS animations
-	once: false, // whether animation should happen only once - while scrolling down
-	mirror: false, // whether elements should animate out while scrolling past them
-	anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
-});
 
 function Home() {
 	const [t] = useTranslation();
@@ -37,11 +14,7 @@ function Home() {
 	return (
 		<section className="home" id="home">
 			<div className="max-width">
-				<div
-					className="home-content"
-					data-aos="fade-right"
-					data-aos-easing="ease-in-out"
-				>
+				<div className="home-content" data-aos="fade-right">
 					<div className="static-txt-1">{t("home.text1")}</div>
 					<div className="static-txt-2">Diogo "Reveratel" Gil</div>
 					<div className="wrapper">
--- Client/src/page/Services.js
+++ Client/src/page/Services.js
@@ -1,10 +1,36 @@
 import React from "react";
+import "./services.css";
+import Line from "../components/Line.js";
+import { Link } from "react-router-dom";
 
 function Services() {
   return (
-    <>
+    <section className="services" data-aos="fade-in">
       <h1>Services</h1>
-    </>
+      <Line />
+      <p>
+        Como designer, eu disponibilizo serviços que podem que encontrem em
+        baixo e poderão requisitar também.
+      </p>
+      <div className="services-container">
+        <div className="services-card">
+          <div className="card-header">{/* Image */}</div>
+          <div className="card-body">
+            <h2>Logotipos</h2>
+            <p>
+              Criação de logos personalizados para fins pessoais e empressial,
+              com acabamento para wallpaper, papel timbrado, estampagem e muitos
+              outros...
+            </p>
+          </div>
+          <div className="card-footer">
+            <label>
+              Saber mais <i className="fas fa-angle-right" />
+            </label>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
