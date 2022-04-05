af790b41bc9739589897356d74c013916a9e86c6
Diogo Gil <diogo.designer.gil@gmail.com>
2022-04-05 12:01:56 +0100

    Scroll to Top pages

--- Client/src/components/AnimatedRoutes.js
+++ Client/src/components/AnimatedRoutes.js
@@ -13,25 +13,28 @@ import ServicesPage from "../page/ServicesPage";
 import ArticlePage from "../page/ArticlePage";
 
 import { AnimatePresence } from "framer-motion/dist/framer-motion";
+import ScrollToTop from "./ScrollToTop";
 
 function AnimatedRoutes() {
 	const location = useLocation();
 	return (
 		<AnimatePresence>
-			<Routes location={location} key={location.pathname}>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/services" element={<Services />} />
-				<Route path="/services/:id" element={<ServicesPage />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/projects/:id" element={<ProjectsPage />} />
-				<Route
-					path="/projects/:id/article/:articleid"
-					element={<ArticlePage />}
-				/>
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Error404 />} />
-			</Routes>
+			<ScrollToTop>
+				<Routes location={location} key={location.pathname}>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/services" element={<Services />} />
+					<Route path="/services/:id" element={<ServicesPage />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/projects/:id" element={<ProjectsPage />} />
+					<Route
+						path="/projects/:id/article/:articleid"
+						element={<ArticlePage />}
+					/>
+					<Route path="/contact" element={<Contact />} />
+					<Route path="*" element={<Error404 />} />
+				</Routes>
+			</ScrollToTop>
 		</AnimatePresence>
 	);
 }
