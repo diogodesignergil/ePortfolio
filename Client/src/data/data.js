fe0f24248f3ddc68ce81e61910ca4b33bb8e6827
Diogo Gil <diogo.designer.gil@gmail.com>
2022-04-05 20:43:01 +0100

    Data.js Update

--- Client/src/data/data.js
+++ Client/src/data/data.js
@@ -4,6 +4,20 @@ import MagicOverlay from "../asset/img/magicoverlay.png";
 import BannerLogo from "../asset/img/LogoBanner.png";
 import BannerOverlay from "../asset/img/OverlayBanner.png";
 
+import Interview from "../asset/img/interview.png";
+import Search from "../asset/img/search.png";
+import Draw from "../asset/img/draw.png";
+import Color from "../asset/img/color.png";
+import Proportion from "../asset/img/proportion.png";
+import Zip from "../asset/img/zip.png";
+import Display from "../asset/img/display.png";
+import BG from "../asset/img/BGOverlay.png";
+import Webcam from "../asset/img/webcam.png";
+import Alert from "../asset/img/alert.png";
+import Stinger from "../asset/img/stinger.png";
+import Test from "../asset/img/test.png";
+import Chat from "../asset/img/chat.png";
+
 export default {
 	skill: [
 		{
@@ -206,6 +220,114 @@ export default {
 			title: "services.overlaycard.title",
 			descrition: "services.overlaycard.descrition",
 			link: "/services/2",
+		},
+	],
+	servicespage: [
+		{
+			id: 1,
+			title: "servicespage.logo.title",
+			info: "servicespage.logo.info",
+			info2: "",
+			step: [
+				{
+					number: "01",
+					title: "servicespage.logo.step1.title",
+					text: "servicespage.logo.step1.text",
+					image: Interview,
+				},
+				{
+					number: "02",
+					title: "servicespage.logo.step2.title",
+					text: "servicespage.logo.step2.text",
+					image: Search,
+				},
+				{
+					number: "03",
+					title: "servicespage.logo.step3.title",
+					text: "servicespage.logo.step3.text",
+					image: Draw,
+				},
+				{
+					number: "04",
+					title: "servicespage.logo.step4.title",
+					text: "servicespage.logo.step4.text",
+					image: Color,
+				},
+				{
+					number: "05",
+					title: "servicespage.logo.step5.title",
+					text: "servicespage.logo.step5.text",
+					image: Proportion,
+				},
+				{
+					number: "06",
+					title: "servicespage.logo.step6.title",
+					text: "servicespage.logo.step6.text",
+					image: Zip,
+				},
+			],
+		},
+		{
+			id: 2,
+			title: "servicespage.overlay.title",
+			info: "servicespage.overlay.info",
+			info2: "servicespage.overlay.info2",
+			step: [
+				{
+					number: "01",
+					title: "servicespage.overlay.step1.title",
+					text: "servicespage.overlay.step1.text",
+					image: Interview,
+				},
+				{
+					number: "02",
+					title: "servicespage.overlay.step2.title",
+					text: "servicespage.overlay.step2.text",
+					image: Display,
+				},
+				{
+					number: "03",
+					title: "servicespage.overlay.step3.title",
+					text: "servicespage.overlay.step3.text",
+					image: BG,
+				},
+				{
+					number: "04",
+					title: "servicespage.overlay.step4.title",
+					text: "servicespage.overlay.step4.text",
+					image: Webcam,
+				},
+				{
+					number: "05",
+					title: "servicespage.overlay.step5.title",
+					text: "servicespage.overlay.step5.text",
+					image: Alert,
+				},
+				{
+					number: "06",
+					title: "servicespage.overlay.step6.title",
+					text: "servicespage.overlay.step6.text",
+					image: Stinger,
+				},
+				{
+					number: "07",
+					title: "servicespage.overlay.step7.title",
+					text: "servicespage.overlay.step7.text",
+					image: Chat,
+				},
+				{
+					number: "08",
+					title: "servicespage.overlay.step8.title",
+					text: "servicespage.overlay.step8.text",
+					image: Test,
+				},
+				{
+					number: "09",
+					title: "servicespage.overlay.step9.title",
+					text: "servicespage.overlay.step9.text",
+					image: Zip,
+				},
+			],
 		},
 	],
 	projects: [
