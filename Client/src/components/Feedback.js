54ca378550efc3943d6c03570df8047e29b36268
Diogo Gil <diogo.designer.gil@gmail.com>
2022-04-09 00:40:33 +0100

    Feeedback Translate Update

--- Client/src/components/Feedback.js
+++ Client/src/components/Feedback.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./feedback.css";
 
+import { useTranslation } from "react-i18next";
+
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react.js";
 import "swiper/swiper-bundle.min.css";
 import "swiper/swiper.scss";
@@ -17,32 +19,34 @@ import data from "../data/data.js";
 const allFeedback = data.feedback;
 
 function Feedback() {
-	return (
-		<Swiper
-			className="feedback-container"
-			// install Swiper modules
-			modules={[Navigation, Scrollbar, Pagination]}
-			autoplay={{ delay: 5000 }}
-			spaceBetween={50}
-			slidesPerView={1}
-			navigation
-			pagination={{ clickable: true }}
-			onSwiper={(swiper) => console.log(swiper)}
-			onSlideChange={() => console.log("slide change")}
-		>
-			{allFeedback.map((feedbackStatus) => {
-				return (
-					<SwiperSlide className="single-feedback" key={feedbackStatus.id}>
-						<div>
-							<img src={feedbackStatus.image} alt={feedbackStatus.altImg} />
-						</div>
-						<h3>{feedbackStatus.name}</h3>
-						<p>"{feedbackStatus.feedback}"</p>
-					</SwiperSlide>
-				);
-			})}
-		</Swiper>
-	);
+  const [t] = useTranslation();
+
+  return (
+    <Swiper
+      className="feedback-container"
+      // install Swiper modules
+      modules={[Navigation, Scrollbar, Pagination]}
+      autoplay={{ delay: 5000 }}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+    >
+      {allFeedback.map((feedbackStatus) => {
+        return (
+          <SwiperSlide className="single-feedback" key={feedbackStatus.id}>
+            <div>
+              <img src={feedbackStatus.image} alt={feedbackStatus.altImg} />
+            </div>
+            <h3>{feedbackStatus.name}</h3>
+            <p>"{t(feedbackStatus.feedback)}"</p>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 }
 
 export default Feedback;
--- Client/src/page/ServicesPage.js
+++ Client/src/page/ServicesPage.js
@@ -19,68 +19,68 @@ import data from "../data/data.js";
 const services = data.servicespage;
 
 function ServicesPage() {
-	const { id } = useParams();
-	const [getServiceData, setService] = useState({});
+  const { id } = useParams();
+  const [getServiceData, setService] = useState({});
 
-	function checkArray(id) {
-		return services.some(function (service) {
-			return service.id === id;
-		});
-	}
+  function checkArray(id) {
+    return services.some(function (service) {
+      return service.id === id;
+    });
+  }
 
-	function getService(id) {
-		return services.find(function (service) {
-			return service.id === parseInt(id);
-		});
-	}
+  function getService(id) {
+    return services.find(function (service) {
+      return service.id === parseInt(id);
+    });
+  }
 
-	const [t] = useTranslation();
+  const [t] = useTranslation();
 
-	useEffect(() => {
-		setService(getService(id));
-	}, []);
+  useEffect(() => {
+    setService(getService(id));
+  }, []);
 
-	return (
-		<motion.div
-			initial={{ y: window.innerHeight, opacity: 0 }}
-			animate={{ y: 0, opacity: 1 }}
-			exit={{
-				y: window.innerHeight,
-				opacity: 0,
-				transition: { duration: 0.1 },
-			}}
-		>
-			{checkArray(parseInt(id)) ? (
-				<section className="servicepage">
-					<h1>{t(getServiceData.title)}</h1>
-					<Line />
-					<p className="info">
-						{t(getServiceData.info)}
-						<br></br>
-						<span className="info2">{t(getServiceData.info2)}</span>
-					</p>
+  return (
+    <motion.div
+      initial={{ y: window.innerHeight, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      exit={{
+        y: window.innerHeight,
+        opacity: 0,
+        transition: { duration: 0.1 },
+      }}
+    >
+      {checkArray(parseInt(id)) ? (
+        <section className="servicepage">
+          <h1>{t(getServiceData.title)}</h1>
+          <Line />
+          <p className="info">
+            {t(getServiceData.info)}
+            <br></br>
+            <span className="info2">{t(getServiceData.info2)}</span>
+          </p>
 
-					{getServiceData.step?.map((step) => {
-						return (
-							<div className="step">
-								<span>{step.number}</span>
-								<div className="text">
-									<h2>{t(step.title)}</h2>
-									<p>{t(step.text)}</p>
-								</div>
-								<img src={step.image} alt="" />
-							</div>
-						);
-					})}
-					<Button buttonStyle="btn--fiverr" buttonSize="btn--large">
-						<img src={FiverrLogo} alt="" />
-					</Button>
-				</section>
-			) : (
-				<Error404 />
-			)}
-		</motion.div>
-	);
+          {getServiceData.step?.map((step) => {
+            return (
+              <div className="step">
+                <span>{step.number}</span>
+                <div className="text">
+                  <h2>{t(step.title)}</h2>
+                  <p>{t(step.text)}</p>
+                </div>
+                <img src={step.image} alt="" />
+              </div>
+            );
+          })}
+          <Button buttonStyle="btn--fiverr" buttonSize="btn--large">
+            <img src={FiverrLogo} alt="" />
+          </Button>
+        </section>
+      ) : (
+        <Error404 />
+      )}
+    </motion.div>
+  );
 }
 
 export default ServicesPage;
